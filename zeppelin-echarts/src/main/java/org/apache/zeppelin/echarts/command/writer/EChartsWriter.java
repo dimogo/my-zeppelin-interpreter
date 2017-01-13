c59adb6ecc0b29fe65debbb0ff2fb180afed71c2
ethan-xiao <308056554@qq.com>
2017-01-13 11:41:38 +0800

    增加velocity模板，调试echarts输出

--- zeppelin-echarts/src/main/java/org/apache/zeppelin/echarts/command/writer/EChartsWriter.java
+++ zeppelin-echarts/src/main/java/org/apache/zeppelin/echarts/command/writer/EChartsWriter.java
@@ -1,9 +1,16 @@
 package org.apache.zeppelin.echarts.command.writer;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.apache.zeppelin.echarts.utils.OutputCombiner;
 import org.apache.zeppelin.echarts.utils.PropertyGetter;
 import org.apache.zeppelin.interpreter.InterpreterContext;
 import org.apache.zeppelin.interpreter.InterpreterResult;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Properties;
 
 /**
  * Created by Ethan Xiao on 2017/1/12.
@@ -11,10 +18,22 @@ import org.apache.zeppelin.interpreter.InterpreterResult;
  */
 public class EChartsWriter extends Writer<String, InterpreterResult> {
 
+	private VelocityEngine ve = new VelocityEngine();
+
 	/**
 	 * 紧跟命令后的HTML代码,附加在前一条命令的结果后一起输出
 	 */
 	private String html;
+
+	public EChartsWriter() {
+		Properties p = new Properties();
+		try {
+			p.load(this.getClass().getResourceAsStream("/velocity.properties"));
+			ve.init(p);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void setParameters(String[] parameters) {
 		//没有参数
@@ -25,15 +44,22 @@ public class EChartsWriter extends Writer<String, InterpreterResult> {
 	}
 
 	public InterpreterResult execute(String input, PropertyGetter propertyGetter, InterpreterContext interpreterContext) {
-		StringBuilder content = new StringBuilder();
-		content.append("<script type=\"text/javascript\" src=\"").append(propertyGetter.getEchartsURL()).append("\"></script>");
-		content.append("<script type=\"text/javascript\" src=\"").append(propertyGetter.getJqeuryURL()).append("\"></script>");
-		//content.append("<link rel=\"stylesheet\" href=\"").append(getBootstrapStyleURL()).append("\" />");
-		//content.append("<link rel=\"stylesheet\" href=\"").append(getBootstrapThemeStyleURL()).append("\" />");
-		content.append("<script type=\"text/javascript\" src=\"").append(propertyGetter.getBootstrapURL()).append("\"></script>");
+		Template template = ve.getTemplate("zeppelin-echarts-body.vm");
+		VelocityContext context = new VelocityContext();
+		context.put("ZeppelinEChartsJSUrl", propertyGetter.getEchartsURL());
+		context.put("ZeppelinEChartsJQueryUrl", propertyGetter.getJqeuryURL());
+		context.put("ZeppelinEChartsBootstrapURL", propertyGetter.getBootstrapURL());
+		context.put("ZeppelinEChartsOriginJsonData", input);
+		context.put("ZeppelinEChartsBodyFoot", this.html);
+		StringWriter writer = new StringWriter();
+		template.merge(context, writer);
+		return new InterpreterResult(InterpreterResult.Code.SUCCESS, InterpreterResult.Type.TEXT, writer.toString());
+	}
 
-		content.append("<link rel=\"stylesheet\" href=\"/plugins/zeppelin-echarts/css/zeppelin-echarts.css\" />");
-		content.append("<script type=\"text/javascript\" src=\"/plugins/zeppelin-echarts/js/zeppelin-echarts.js\"></script>");
-		return new InterpreterResult(InterpreterResult.Code.SUCCESS, InterpreterResult.Type.HTML, OutputCombiner.combine(content, input, this.html));
+	public static void main(String[] args) {
+		EChartsWriter writer = new EChartsWriter();
+		String originData = null;
+		InterpreterResult result = writer.execute(originData, null, null);
+		System.out.println(result.toString());
 	}
 }
