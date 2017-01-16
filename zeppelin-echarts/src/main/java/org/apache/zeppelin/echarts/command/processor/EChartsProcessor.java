38c169d43a5939c5266cdcd902f6725815cf1d37
ethan-xiao <308056554@qq.com>
2017-01-16 22:20:24 +0800

    调整serie选择区域

--- zeppelin-echarts/src/main/java/org/apache/zeppelin/echarts/command/processor/EChartsProcessor.java
+++ zeppelin-echarts/src/main/java/org/apache/zeppelin/echarts/command/processor/EChartsProcessor.java
@@ -1,22 +1,15 @@
 package org.apache.zeppelin.echarts.command.processor;
 
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.exception.ResourceNotFoundException;
-import org.apache.zeppelin.echarts.command.writer.Writer;
-import org.apache.zeppelin.echarts.utils.OutputCombiner;
 import org.apache.zeppelin.echarts.utils.PropertyGetter;
 import org.apache.zeppelin.interpreter.InterpreterContext;
-import org.apache.zeppelin.interpreter.InterpreterResult;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * Created by Ethan Xiao on 2017/1/12.
@@ -27,6 +20,7 @@ public class EChartsProcessor extends Processor<String, String> {
 	private VelocityEngine ve = new VelocityEngine();
 	private Map<String, String> optionSettings = new HashMap<String, String>();
 	private int seriesCount = 1;
+	private List<Serie> serieList = new LinkedList<Serie>();
 
 	/**
 	 * 紧跟命令后的HTML代码,附加在前一条命令的结果后一起输出
@@ -51,6 +45,9 @@ public class EChartsProcessor extends Processor<String, String> {
 	}
 
 	public void setParameters(String[] parameters) {
+		if (parameters == null) {
+			return;
+		}
 		if (parameters.length > 0) {
 			this.seriesCount = Integer.parseInt(parameters[0]);
 		}
@@ -63,6 +60,19 @@ public class EChartsProcessor extends Processor<String, String> {
 			} else {
 				optionSettings.put(options[0], body);
 			}
+		} else if ("serie".equalsIgnoreCase(name) && options != null && options.length >= 4) {
+			Serie serie = new Serie();
+			serie.index = Integer.parseInt(options[0]);
+			serie.name = options[1];
+			serie.type = options[2];
+			serie.fields = options[3];
+			if (options.length >= 5) {
+				serie.xAxisIndex = Integer.parseInt(options[4]);
+			}
+			if (options.length >= 6) {
+				serie.yAxisIndex = Integer.parseInt(options[5]);
+			}
+			this.serieList.add(serie);
 		}
 	}
 
@@ -88,11 +98,28 @@ public class EChartsProcessor extends Processor<String, String> {
 			context.put("ZeppelinEChartsBodyFoot", this.html);
 			context.put("ZeppelinEchartsOptionSettings", optionSettings);
 			context.put("ZeppelinEchartsSeriesCount", this.seriesCount);
+			context.put("ZeppelinEchartsSerieList", this.serieList);
 			StringWriter writer = new StringWriter();
 			template.merge(context, writer);
 			return writer.toString();
 		} catch (ResourceNotFoundException e) {
 			throw new RuntimeException("vm path:" + this.vmPath, e);
+		}
+	}
+
+	private static class Serie {
+		private int index = 0;
+		private String name = "";
+		private String type = "line";
+		private String fields = "";
+		private int xAxisIndex = 0;
+		private int yAxisIndex = 0;
+
+		public String toString() {
+			return "{index:" + index + ",name:\"" + name + "\",type:\"" + type + "\",fields:\"" + fields
+					+ "\",xAxisIndex:" + xAxisIndex
+					+ ",yAxisIndex:" + yAxisIndex
+					+ "}";
 		}
 	}
 
