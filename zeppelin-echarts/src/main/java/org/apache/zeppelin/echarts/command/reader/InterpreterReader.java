327e62c5d930adb2c57914258b80958f31e90dc7
ethan-xiao <308056554@qq.com>
2017-02-02 16:47:07 +0800

    增加解释器调用逻辑

--- zeppelin-echarts/src/main/java/org/apache/zeppelin/echarts/command/reader/InterpreterReader.java
+++ zeppelin-echarts/src/main/java/org/apache/zeppelin/echarts/command/reader/InterpreterReader.java
@@ -1,14 +1,26 @@
 package org.apache.zeppelin.echarts.command.reader;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.zeppelin.echarts.utils.PropertyGetter;
+import org.apache.zeppelin.interpreter.Interpreter;
 import org.apache.zeppelin.interpreter.InterpreterContext;
+import org.apache.zeppelin.interpreter.InterpreterResult;
+import org.apache.zeppelin.interpreter.InterpreterResultMessage;
+import org.apache.zeppelin.server.ZeppelinServer;
+
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by Ethan Xiao on 2017/1/12.
  */
 public class InterpreterReader extends Reader<String, String> {
-	public void setParameters(String[] parameters) {
+	private String replName;
+	private String body;
 
+	public void setParameters(String[] parameters) {
+		if (parameters != null && parameters.length > 0) {
+			this.replName = parameters[0];
+		}
 	}
 
 	public void addPara(String name, String[] options, String body) {
@@ -16,10 +28,19 @@ public class InterpreterReader extends Reader<String, String> {
 	}
 
 	public void setBody(String body) {
-
+		this.body = body;
 	}
 
 	public String execute(String input, PropertyGetter propertyGetter, InterpreterContext interpreterContext) {
-		return null;
+		try {
+			InterpreterContext subContext = (InterpreterContext) BeanUtils.cloneBean(interpreterContext);
+			BeanUtils.setProperty(subContext, "replName", this.replName);
+			Interpreter interpreter = ZeppelinServer.notebook.getInterpreterFactory().getInterpreter(interpreterContext
+					.getAuthenticationInfo().getUser(), interpreterContext.getNoteId(), subContext.getReplName());
+			InterpreterResult rs = interpreter.interpret(this.body, subContext);
+			return rs.toString();
+		} catch (Exception e) {
+			throw new RuntimeException("call sub interpreter error:", e);
+		}
 	}
 }
